191a602c034e48c54b4276ed141434bd3945cf6e
ilyankou <ilyankou@gmail.com>
2019-01-14 16:37:38 -0500

    Update with ACS2017 5y estimates

--- scripts/Poverty-Status-by-Town-processing.R
+++ scripts/Poverty-Status-by-Town-processing.R
@@ -1,4 +1,6 @@
 library(dplyr)
+library(devtools)
+load_all('../datapkg')
 library(datapkg)
 library(acs)
 library(stringr)
@@ -17,7 +19,7 @@ source('./scripts/acsHelpers.R')
 
 #Get state data
 geography=geo.make(state=09)
-yearlist=c(2009:2016)
+yearlist=c(2017:2017)
 span = 5
 col.names="pretty" 
 key="ed0e58d2538fb239f51e01643745e83f380582d7"
@@ -313,7 +315,7 @@ dataset <- dataset %>%
 
 write.table(
     dataset,
-    file.path("data", "poverty-status-by-town-2016.csv"),
+    file.path("data", "poverty-status-by-town-TEST.csv"),
     sep = ",",
     row.names = F,
     na = "-9999"
