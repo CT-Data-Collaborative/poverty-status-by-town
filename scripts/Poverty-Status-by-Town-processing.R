1af214f42db414778f0d8132dc9fe4b061380a28
ilyankou <ilyankou@gmail.com>
2021-03-02 17:08:29 +0000

    Update ACS 2019 5y

--- scripts/Poverty-Status-by-Town-processing.R
+++ scripts/Poverty-Status-by-Town-processing.R
@@ -19,7 +19,7 @@ source('./scripts/acsHelpers.R')
 
 #Get state data
 geography=geo.make(state=09)
-yearlist=c(2009:2018)
+yearlist=c(2009:2019)
 span = 5
 col.names="pretty" 
 key="ed0e58d2538fb239f51e01643745e83f380582d7"
@@ -315,7 +315,7 @@ dataset <- dataset %>%
 
 write.table(
     dataset,
-    file.path("data", "poverty-status-by-town-2018.csv"),
+    file.path("data", "poverty-status-by-town-2019.csv"),
     sep = ",",
     row.names = F,
     na = "-9999"
